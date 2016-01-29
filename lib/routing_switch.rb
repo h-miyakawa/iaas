0b5dff686d1c9ae9691b7e2b76600b79cbfe40b8
MIYAKAWA Hirotaka <h-miyakawa@ist.osaka-u.ac.jp>
2016-01-29 18:32:28 +0900

    Finish methods for firewall entry

--- lib/routing_switch.rb
+++ lib/routing_switch.rb
@@ -47,8 +47,21 @@ class RoutingSwitch < Trema::Controller
   def packet_in(dpid, message)
     @topology.packet_in(dpid, message)
     @path_manager.packet_in(dpid, message) unless message.lldp?
-    # print message
-    add_block_entry(message.source_ip_address, message.destination_ip_address, 100) unless message.lldp?
+      
+    unless message.lldp? then
+      options = {}
+      options[:ether_type] = 0x0800
+      options[:source_ip_address] =  message.source_ip_address
+      # options[:destination_ip_address] = message.destination_ip_address
+      add_block_entry(
+        100,
+        options
+      )
+      delete_firewall_entry(
+        100,
+        options
+      )
+    end
   end
 
   private
@@ -70,47 +83,53 @@ class RoutingSwitch < Trema::Controller
     end
   end
 
-
-
-  # def delete_firewall_flow_entry()
-  # end # delete_firewall_flow_entry
-
-  # black list
-#   def add_firewall_flow_entry()
-#       send_flow_mod_add(
-#         datapath_id,
-#         match: Match.new(
-#           ip_source_address: src_ip_for_blocking,
-#           ip_destination_address: dest_ip_for_blocking,
-#           transport_source_port: src_port_for_blocking,
-#           transport_destination_port: dest_port_for_blocking
-#         )
-#       )
-#   end # add_firewall_flow_entry
-
-  def add_block_entry(
-    ip_src,
-    ip_dst,
-    priority
-    )
-
-    # get datapath id from ip address
-    hosts = topology.hosts.each_with_object({}) do |host, tmp|
+  def get_host_info_from_ip(ip_addr)
+    hosts = @path_manager.topology.hosts.each_with_object({}) do |host, tmp|
       mac_address, ip_address, dpid, port_no = *host
-      if ip_address = ip_src then
-        datapath_id = dpid
+      if ip_address == ip_addr then
+        host_info = {}
+        host_info[:dpid] = dpid
+        host_info[:port_no] = port_no
+        return host_info
       end
     end
+  end
+
+  def delete_firewall_entry(
+    priority,
+    options
+  )
+    ip_src = options[:source_ip_address]
+    # get datapath id from ip address
+    host_info = get_host_info_from_ip(ip_src)
+    @_dpid = host_info[:dpid]
 
     # add flow entry
-    # oldfathioned? nw_dst: ip_dst, dl_type: 0x0800
-    send_flow_mod_add(
-      datapath_id,
+    # TCP: ip_protocol:6 
+    send_flow_mod_delete(
+      @_dpid,
       priority: priority,
-      match: Match.new(
-        ip_source_address: ip_src,
-        ip_destination_address: ip_dst,
-      )
+      match: Match.new(options),
+    )
+  end # delete_firewall_flow_entry
+
+  def add_block_entry(
+    priority,
+    options
+  )
+
+    ip_src = options[:source_ip_address]
+    # get datapath id from ip address
+    host_info = get_host_info_from_ip(ip_src)
+    @_dpid = host_info[:dpid]
+
+    # add flow entry
+    # TCP: ip_protocol:6 
+    send_flow_mod_add(
+      @_dpid,
+      priority: priority,
+      match: Match.new(options),
+      # actions: SendOutPort.new(@port)
     )
   end
 end
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ sudo rm /tmp/*
 for i in 1 2 3 4; do
   sudo ovs-vsctl del-br brswitch${i}
 done
-net_stop
+sudo service network-manager stop
 ./bin/trema run lib/routing_switch.rb -c trema.conf -- -s graphviz
-net_start
+sudo service network-manager start
 exit
